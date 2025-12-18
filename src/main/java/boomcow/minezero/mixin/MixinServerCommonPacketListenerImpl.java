ce79e93465d4a9f1cef74269cbe5d2ebada0c4ed
Reset171122 <58262801+Reset171122@users.noreply.github.com>
2025-12-18 16:28:26 +0300

    Update fix

--- src/main/java/boomcow/minezero/mixin/MixinServerCommonPacketListenerImpl.java
+++ src/main/java/boomcow/minezero/mixin/MixinServerCommonPacketListenerImpl.java
@@ -3,14 +3,14 @@ package boomcow.minezero.mixin;
 import boomcow.minezero.checkpoint.CheckpointManager;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundExplodePacket;
-import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.server.network.ServerCommonPacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ServerGamePacketListenerImpl.class)
-public class MixinServerGamePacketListenerImpl {
+@Mixin(ServerCommonPacketListenerImpl.class)
+public class MixinServerCommonPacketListenerImpl {
 
     @Inject(method = "send(Lnet/minecraft/network/protocol/Packet;)V", at = @At("HEAD"), cancellable = true)
     private void onSend(Packet<?> packet, CallbackInfo ci) {
