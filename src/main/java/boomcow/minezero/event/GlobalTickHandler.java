6558838bf469c052974c183c8112330e60d038a2
Reset171122 <58262801+Reset171122@users.noreply.github.com>
2025-12-18 15:57:22 +0300

    Update fix

--- src/main/java/boomcow/minezero/event/GlobalTickHandler.java
+++ src/main/java/boomcow/minezero/event/GlobalTickHandler.java
@@ -1,20 +1,19 @@
 package boomcow.minezero.event;
 
+import boomcow.minezero.checkpoint.CheckpointManager;
 import boomcow.minezero.util.LightningScheduler;
 import net.minecraft.server.level.ServerLevel;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.tick.ServerTickEvent;
 import net.neoforged.neoforge.server.ServerLifecycleHooks;
 
-
 public class GlobalTickHandler {
 
     @SubscribeEvent
     public static void onServerTick(ServerTickEvent.Post event) {
-
         for (ServerLevel level : ServerLifecycleHooks.getCurrentServer().getAllLevels()) {
             LightningScheduler.tick(level);
         }
-
+        CheckpointManager.wasRestoredThisTick = false;
     }
 }
