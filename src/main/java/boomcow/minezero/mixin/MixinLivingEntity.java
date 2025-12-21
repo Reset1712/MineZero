dce22c88d6b38a3232a7861afdcac5d6bd35e981
Reset171122 <58262801+Reset171122@users.noreply.github.com>
2025-12-22 02:48:57 +0300

    Fix MixinLivingEntity.java

--- src/main/java/boomcow/minezero/mixin/MixinLivingEntity.java
+++ src/main/java/boomcow/minezero/mixin/MixinLivingEntity.java
@@ -1,16 +1,23 @@
 package boomcow.minezero.mixin;
 
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.LootTable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
+import java.util.function.Consumer;
 
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity extends Entity {
@@ -37,10 +44,15 @@ public abstract class MixinLivingEntity extends Entity {
         }
     }
 
-    @Inject(method = "dropFromLootTable", at = @At("HEAD"))
-    private void onDropFromLootTable(DamageSource damageSource, boolean hitByPlayer, CallbackInfo ci) {
+    @Redirect(method = "dropFromLootTable", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/storage/loot/LootTable;getRandomItems(Lnet/minecraft/world/level/storage/loot/LootParams;Ljava/util/function/Consumer;)V"))
+    private void redirectLootGeneration(LootTable table, LootParams params, Consumer<ItemStack> consumer) {
         if (this.minezero$lootSeed != 0) {
-            this.random.setSeed(this.minezero$lootSeed);
+            LootContext.Builder builder = new LootContext.Builder(params);
+            builder.withOptionalRandomSeed(this.minezero$lootSeed);
+            LootContext context = builder.create(Optional.empty());
+            table.getRandomItems(context, consumer);
+        } else {
+            table.getRandomItems(params, consumer);
         }
     }
 }
