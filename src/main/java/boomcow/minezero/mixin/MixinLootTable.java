1ef05afb28d8354a9e3fc02a864fefbce4473d0f
Reset171122 <58262801+Reset171122@users.noreply.github.com>
2025-12-22 14:06:02 +0300

    Deterministic loot

---
+++ src/main/java/boomcow/minezero/mixin/MixinLootTable.java
@@ -0,0 +1,42 @@
+package boomcow.minezero.mixin;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+
+@Mixin(LootTable.class)
+public abstract class MixinLootTable {
+
+    @Shadow
+    public abstract void getRandomItems(LootContext context, Consumer<ItemStack> stackConsumer);
+
+    @Inject(method = "getRandomItems(Lnet/minecraft/world/level/storage/loot/LootParams;Ljava/util/function/Consumer;)V", at = @At("HEAD"), cancellable = true)
+    private void onGetRandomItems(LootParams params, Consumer<ItemStack> consumer, CallbackInfo ci) {
+        Entity entity = params.getOptionalParameter(LootContextParams.THIS_ENTITY);
+
+        if (entity instanceof LivingEntity && !(entity instanceof Player)) {
+            long seed = entity.getUUID().getMostSignificantBits() ^ entity.getUUID().getLeastSignificantBits();
+            
+            LootContext.Builder builder = new LootContext.Builder(params);
+            builder.withOptionalRandomSeed(seed);
+            
+            LootContext context = builder.create(Optional.empty());
+            
+            this.getRandomItems(context, consumer);
+            ci.cancel();
+        }
+    }
+}
