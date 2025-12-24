db01e773615f98556952913639bd673248cc2036
Reset1712 <58262801+Reset1712@users.noreply.github.com>
2025-12-25 00:57:07 +0300

    Update MixinEntity.java

--- src/main/java/boomcow/minezero/mixin/MixinEntity.java
+++ src/main/java/boomcow/minezero/mixin/MixinEntity.java
@@ -3,6 +3,7 @@ package boomcow.minezero.mixin;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -22,12 +23,25 @@ public abstract class MixinEntity {
     @Shadow
     public abstract UUID getUUID();
 
+    @Shadow public abstract Level level();
+
     @Inject(method = "load", at = @At("TAIL"))
     private void onLoad(CompoundTag tag, CallbackInfo ci) {
         UUID uuid = this.getUUID();
         if (uuid != null) {
             long seed = uuid.getMostSignificantBits() ^ uuid.getLeastSignificantBits();
             this.random = RandomSource.create(seed);
+        }
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void onTick(CallbackInfo ci) {
+        if (!this.level().isClientSide) {
+            UUID uuid = this.getUUID();
+            if (uuid != null) {
+                long seed = (uuid.getMostSignificantBits() ^ uuid.getLeastSignificantBits()) ^ (this.level().getGameTime() * 25214903917L);
+                this.random.setSeed(seed);
+            }
         }
     }
 }
