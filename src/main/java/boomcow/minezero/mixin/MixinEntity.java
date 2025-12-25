adcd0add679593f5366e38a630df9ea40aafe548
Reset1712 <58262801+Reset1712@users.noreply.github.com>
2025-12-25 16:17:19 +0300

    Undo

--- src/main/java/boomcow/minezero/mixin/MixinEntity.java
+++ src/main/java/boomcow/minezero/mixin/MixinEntity.java
@@ -1,6 +1,5 @@
 package boomcow.minezero.mixin;
 
-import boomcow.minezero.util.TimeLockedRandom;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
@@ -12,19 +11,23 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.UUID;
+
 @Mixin(Entity.class)
 public abstract class MixinEntity {
 
     @Shadow @Final @Mutable
     public RandomSource random;
 
+    @Shadow
+    public abstract UUID getUUID();
+
     @Inject(method = "load", at = @At("TAIL"))
     private void onLoad(CompoundTag tag, CallbackInfo ci) {
-        this.random = new TimeLockedRandom((Entity) (Object) this);
-    }
-    
-    @Inject(method = "<init>", at = @At("TAIL"))
-    private void onInit(CallbackInfo ci) {
-        this.random = new TimeLockedRandom((Entity) (Object) this);
+        UUID uuid = this.getUUID();
+        if (uuid != null) {
+            long seed = uuid.getMostSignificantBits() ^ uuid.getLeastSignificantBits();
+            this.random = RandomSource.create(seed);
+        }
     }
 }
